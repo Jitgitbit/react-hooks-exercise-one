4bf9be767d1725870c703fc21b33d203dbae84f2
Jitgitbit <rowowolf@gmail.com>
2020-03-24 17:45:02 +0100

    SongList created with HC

---
+++ src/components/SongList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SongList() {
+  return (
+    <div>
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  )
+}
