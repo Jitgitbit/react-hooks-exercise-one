eb167678c52b1d672250c8b97e419b4f846b03dd
Jitgitbit <rowowolf@gmail.com>
2020-03-24 17:51:48 +0100

    useState defined

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function SongList() {
+  const [songs, setSongs] = useState([
+    {title: 'almost home', id: 1},
+    {title: 'memory gospel', id: 2},
+    {title: 'this wild darkness', id: 3}
+  ])
   return (
     <div>
       <ul>
